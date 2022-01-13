880da6ff503d55b177e99a7cd7bad672866c60ed
Jackson G. Kaindume <seestem@merely.tech>
2022-01-13 19:52:44 +0200

    [solana]: convert to async and use genrics

--- src/solana.rs
+++ src/solana.rs
@@ -1,14 +1,19 @@
+//! Solana heed
+
 use crate::{DecentEntity, DecentNetState, Heeder};
+use async_trait::async_trait;
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::pubkey::Pubkey;
 use std::str::FromStr;
 
 /// Represents the state of the Solana blockchain
 pub struct SolanaState;
+
 /// Solana blockchain heeder
 pub struct SolanaHeed {
     rpc_url: String,
 }
+
 impl Default for SolanaHeed {
     fn default() -> Self {
         SolanaHeed {
@@ -16,9 +21,11 @@ impl Default for SolanaHeed {
         }
     }
 }
-impl Heeder for SolanaHeed {
+
+#[async_trait]
+impl Heeder<u64> for SolanaHeed {
     /// heed solana
-    fn heed(&self, to_heed: Vec<String>) -> Vec<DecentEntity> {
+    async fn heed(&self, to_heed: Vec<String>) -> Vec<DecentEntity<u64>> {
         let rpc_client = RpcClient::new(String::from(&self.rpc_url));
         let latest_block = rpc_client.get_latest_blockhash().unwrap();
         let pubkeys: Vec<Pubkey> = to_heed
@@ -26,13 +33,13 @@ impl Heeder for SolanaHeed {
             .map(|l| Pubkey::from_str(&l).unwrap())
             .collect();
         let accounts = rpc_client.get_multiple_accounts(&pubkeys).unwrap();
-        let mut entities: Vec<DecentEntity> = vec![];
+        let mut entities: Vec<DecentEntity<u64>> = vec![];
 
         for i in 0..accounts.len() {
             match &accounts[i] {
                 Some(a) => {
                     // convert solana Account to a DecentEntity
-                    let mut entity: DecentEntity = a.clone().into();
+                    let mut entity: DecentEntity<u64> = a.clone().into();
                     entity.location = to_heed[i].clone();
                     // set network state
                     entity.net_state = DecentNetState {
